cbf7701247442c69b09754cb2af6a73ee9c6db11
Claudia Vidican <cvidican@quicksuitetrading.com>
2024-05-17 15:55:52 +0300

    modify db variables

--- products-endpoint/src/main/resources/data.sql
+++ products-endpoint/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT into products.product (id,name,price) VALUES ('1000', 'Apple', '5');
+INSERT IGNORE into products.product (id,name,price) VALUES ('1000', 'Apple', '5');
+INSERT IGNORE into products.product (id,name,price) VALUES ('1001', 'Cream', '10');
+INSERT IGNORE into products.product (id,name,price) VALUES ('1002', 'Bicycle', '200');
